7a7f028d7c87889b4a355b465e13793e39a7005e
xelopsys <juraqulovjamshid7@gmail.com>
2022-07-17 09:03:21 +0500

    bugs are fixed and optimized

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,22 +18,22 @@ export default function Navbar() {
 	return (
 		<nav className="bg-white border-gray-200 px-2 md:px-4 py-2.5 ">
 			<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-				<a href="https://flowbite.com" className="flex items-center">
+				<a href="https://novalabtech.com/" className="flex items-center">
 					<Logo />
 				</a>
 				<div className="flex items-center md:order-2">
-					<a
-						href="/login"
+					<NavLink
+						to="/login"
 						className="text-gray-800  hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2  cus:ring-gray-800"
 					>
 						Sign in
-					</a>
-					<a
-						href="/register"
+					</NavLink>
+					<NavLink
+						to="/register"
 						className="text-white bg-[#1E6DEB] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 focus:outline-none "
 					>
 						Sign up
-					</a>
+					</NavLink>
 				</div>
 				<div className="hidden justify-between items-center w-full text-sm md:flex md:w-auto md:order-1">
 					<ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
