fcdb10498bf17a1c5cfdbee1e2f32f17df972a82
xelopsys <juraqulovjamshid7@gmail.com>
2022-07-17 09:00:44 +0500

    bugs are fixed

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { register } from "../../app/features/userSlice";
 import axios from "axios";
